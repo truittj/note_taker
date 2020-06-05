aa5d8118e4bf9689fc5e4713b94c522f2e1c21fd
gatekeeper <63681296+truittj@users.noreply.github.com>
2020-06-05 08:53:28 -0500

    API routes are working

---
+++ server.js
@@ -0,0 +1,40 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//sever static content for the app from the "public" directory in the app
+app.use(express.static("public"));
+// Tables (DATA)
+// =============================================================
+var notes = [
+  {
+    // customerName: "Ahmed",
+    // customerEmail: "afhaque89@gmail.com",
+    // customerID: "afhaque89",
+    // phoneNumber: "979-587-0887"
+  }
+];
+
+
+
+
+// Routes
+// =============================================================
+require("./routes/apiroutes")(app);
+require("./routes/htmlroutes")(app);
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on: http://localhost:" + PORT);
+});
